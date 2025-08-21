6600a54d18614b303fb3a6d882adc8d2531daa99
Antonino Calisto dos Santos Neto - PrestServ <antonino.santos.prestserv@petrobras.com.br>
2025-08-21 14:26:13 -0300

    feat: ajustando boas praticas de typescript

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -5,10 +5,13 @@ type Props = {
   children?: React.ReactNode;
 };
 
-class Botao extends React.Component<Props> {
+class Botao extends React.Component<{
+  type?: "button" | "submit" | "reset" | undefined
+}> {
   render() {
+    const { type = "button" } = this.props;
     return (
-      <button className={style.botao}>
+      <button type={type} className={style.botao}>
         {this.props.children}
       </button>
     )
